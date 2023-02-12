251e81612050e57f1b12562e0dbf9b5c1b50d0c2
= <=>
2023-02-12 02:36:45 +0200

    css

--- script.js
+++ script.js
@@ -1,47 +1,13 @@
-const canvas = document.getElementById("image-canvas");
-const ctx = canvas.getContext("2d");
-const testButton = document.getElementById("test-button");
+const segCount = 6;
 
-canvas.height = 600;
-canvas.width = 800;
-
-testButton.addEventListener("click", () => {
-    drawCropOverlay(ctx, 10, 50, 600, 300, 5, 5);
-});
-
-function drawCropOverlay(ctx, x, y, width, height, horizontalSegmentCount, verticalSegmentCount) {
-    drawMainRectangle(ctx, x, y, width, height);
-    drawSegments(ctx, x, y, width, height, horizontalSegmentCount, true);
-    drawSegments(ctx, x, y, width, height, verticalSegmentCount, false);
-    // drawReshapeButton(ctx, x + width, y + width);
-    drawReshapeButton(ctx, width + x, height + y);
-}
-
-function drawMainRectangle(ctx, x, y, width, height) {
-    ctx.beginPath();
-    ctx.fillStyle = "rgba(32, 45, 21, 0.3)";
-    ctx.rect(x, y, width, height);
-    ctx.fill();
-}
-
-function drawSegments(ctx, x, y, width, height, segmentCount, isHorizontal) {
-    const segmentSize = (isHorizontal ? width : height) / segmentCount;
-    console.log(isHorizontal ? "h" : "v");
-    console.log(`seg size: ${segmentSize}`);
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    for (let step = 1; step < segmentCount; step++) {
-        let segmentPosition = (isHorizontal ? x : y) + segmentSize * step;
-        console.log(segmentPosition);
-        ctx.moveTo(isHorizontal ? segmentPosition : x, isHorizontal ? y : segmentPosition);
-        ctx.lineTo(isHorizontal ? segmentPosition : x + width, isHorizontal ? y + height : segmentPosition);
+document.addEventListener("DOMContentLoaded", () => {
+    const one = document.querySelector("div");
+    const two = document.createElement("div");
+    two.className = "two";
+    for (let i = 0; i < segCount; i++) {
+        const twoB = two.cloneNode(false);
+        twoB.style.left = `${100 * i}px`;
+        one.appendChild(twoB);  
     }
-    ctx.stroke();
-}
-
-function drawReshapeButton(ctx, x, y) {
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, 2 * Math.PI);
-    ctx.fillStyle = "black";
-    ctx.fill();
-}
+    
+});
